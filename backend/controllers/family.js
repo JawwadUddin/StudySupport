b9525946e30526ac89bc04e576cdd8af9a474e14
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-10-04 22:41:26 +0100

    add family controller for family route

---
+++ backend/controllers/family.js
@@ -0,0 +1,44 @@
+const Family = require("../models/Family");
+const { success, error } = require("../helper/responseApi");
+
+async function index(req, res) {
+  try {
+    const families = await Family.all;
+    res.status(200).json(success("OK", { data: families }, res.statusCode));
+  } catch (err) {
+    res.status(500).json(error(err, res.statusCode));
+  }
+}
+
+async function show(req, res) {
+  const familyID = req.params.id;
+  try {
+    const family = await Family.findByID(familyID);
+    res.status(200).json(success("OK", { data: family }, res.statusCode));
+  } catch (err) {
+    res.status(404).json(error(err, res.statusCode));
+  }
+}
+
+async function create(req, res) {
+  const submittedFamily = req.body.data;
+  try {
+    const family = await Family.create(submittedFamily);
+    res.status(201).json(success("OK", { data: family }, res.statusCode));
+  } catch (err) {
+    res.status(500).json(error(err, res.statusCode));
+  }
+}
+
+async function update(req, res) {
+  const id = req.params.id;
+  const submittedFamily = req.body.data;
+  try {
+    const family = await Family.edit(id, submittedFamily);
+    res.status(200).json(success("OK", { data: family }, res.statusCode));
+  } catch (err) {
+    res.status(500).json(error(err, res.statusCode));
+  }
+}
+
+module.exports = { index, show, create, update };
