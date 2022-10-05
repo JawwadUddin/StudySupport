5638a4f0cffaf4067e23f41073d8a227477273ea
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-10-05 22:38:07 +0100

    create student controller

---
+++ backend/controllers/student.js
@@ -0,0 +1,44 @@
+const Student = require("../models/Student");
+const { success, error } = require("../helper/responseApi");
+
+async function index(req, res) {
+  try {
+    const students = await Student.all;
+    res.status(200).json(success("OK", { data: students }, res.statusCode));
+  } catch (err) {
+    res.status(500).json(error(err, res.statusCode));
+  }
+}
+
+async function show(req, res) {
+  const studentID = req.params.id;
+  try {
+    const student = await Student.findByID(studentID);
+    res.status(200).json(success("OK", { data: student }, res.statusCode));
+  } catch (err) {
+    res.status(404).json(error(err, res.statusCode));
+  }
+}
+
+async function create(req, res) {
+  const submittedStudent = req.body.data;
+  try {
+    const student = await Student.create(submittedStudent);
+    res.status(201).json(success("OK", { data: student }, res.statusCode));
+  } catch (err) {
+    res.status(500).json(error(err, res.statusCode));
+  }
+}
+
+async function update(req, res) {
+  const studentId = req.params.id;
+  const submittedStudent = req.body.data;
+  try {
+    const student = await Student.edit(studentId, submittedStudent);
+    res.status(200).json(success("OK", { data: student }, res.statusCode));
+  } catch (err) {
+    res.status(500).json(error(err, res.statusCode));
+  }
+}
+
+module.exports = { index, show, create, update };
