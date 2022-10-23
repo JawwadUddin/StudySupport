58e88d2e9c528aaa1a59e234d9c7dc48df8fc422
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-10-23 20:01:30 +0100

    create sidebar layout component and styling

---
+++ frontend/src/layout/sidebar/Sidebar.jsx
@@ -0,0 +1,43 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import QuizIcon from "@mui/icons-material/Quiz";
+import { useNavigate } from "react-router-dom";
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">StudySupport</span>
+      </div>
+      <div className="center">
+        <ul>
+          <li onClick={() => navigate("")}>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <li onClick={() => navigate("contacts")}>
+            <ImportContactsIcon className="icon" />
+            <span>Contacts</span>
+          </li>
+          <li onClick={() => navigate("students")}>
+            <PersonOutlineOutlinedIcon className="icon" />
+            <span>Students</span>
+          </li>
+          <li onClick={() => navigate("tests")}>
+            <QuizIcon className="icon" />
+            <span>Tests</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
