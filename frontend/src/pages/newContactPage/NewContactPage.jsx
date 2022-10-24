6000164d11f2ebe36e9aa4ba9ffe947f8ccbcdf7
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-10-24 17:45:08 +0100

    Stubbed new contact page

---
+++ frontend/src/pages/newContactPage/NewContactPage.jsx
@@ -0,0 +1,15 @@
+import "./newContactPage.scss";
+import ContactForm from "../../components/contactForm/ContactForm";
+
+const NewContactPage = () => {
+  return (
+    <div className="newContactPageContainer">
+      <h2 className="title">New Contact</h2>
+      <div className="formContainer">
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default NewContactPage;
