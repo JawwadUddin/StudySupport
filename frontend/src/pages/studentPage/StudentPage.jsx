41254fa5170543d865f23a0e968ec0fc44505eb7
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-10-24 17:45:53 +0100

    create student page and styling

--- frontend/src/pages/studentPage/StudentPage.jsx
+++ frontend/src/pages/studentPage/StudentPage.jsx
@@ -1,5 +1,47 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./studentPage.scss";
+import Table from "../../components/table/Table";
+import { getData } from "../../helpers/apiFunctions";
+import Button from "@mui/material/Button";
+
 const StudentPage = () => {
-  return <div>StudentPage</div>;
+  const [students, setStudents] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    try {
+      async function fetchData() {
+        const serverResponse = await getData(
+          "http://localhost:5000/api/student"
+        );
+        if (serverResponse.message === "OK") {
+          setStudents(serverResponse.results.data);
+        }
+      }
+      fetchData();
+      console.log(students);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <div className="studentPageContainer">
+      <h2 className="title">Students</h2>
+      <div className="listContainer">
+        <div className="listHeader">
+          <div className="listTitle">All Students</div>
+          <Button
+            onClick={() => navigate("/students/new")}
+            variant="contained"
+            className="createBtn"
+          >
+            Create New Student
+          </Button>
+        </div>
+        <Table data={students} type="student" />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
