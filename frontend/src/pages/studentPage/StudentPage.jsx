03e174532534cda9d8190d103eb8f26bee218ac2
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-10-26 00:30:15 +0100

    modal and delete functionality for students

--- frontend/src/pages/studentPage/StudentPage.jsx
+++ frontend/src/pages/studentPage/StudentPage.jsx
@@ -2,12 +2,21 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./studentPage.scss";
 import Table from "../../components/table/Table";
-import { getData } from "../../helpers/apiFunctions";
+import { getData, deleteData } from "../../helpers/apiFunctions";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
 
 const StudentPage = () => {
   const [students, setStudents] = useState([]);
+  const [refresh, setRefresh] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [deleteID, setDeleteID] = useState(null);
+  const [deleteText, setDeleteText] = useState("");
+
   const navigate = useNavigate();
+
   useEffect(() => {
     try {
       async function fetchData() {
@@ -16,14 +25,53 @@ const StudentPage = () => {
         );
         if (serverResponse.message === "OK") {
           setStudents(serverResponse.results.data);
+          setRefresh(false);
+        } else {
+          throw Error(serverResponse.message);
         }
       }
-      fetchData();
-      console.log(students);
+      if (refresh) {
+        fetchData();
+      }
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [refresh]);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setDeleteID(null);
+    setDeleteText("");
+    setOpen(false);
+  };
+
+  const handleConfirmDelete = () => {
+    if (deleteText !== "delete") {
+      console.log("Will not delete");
+      return;
+    }
+    async function removeData() {
+      const serverResponse = await deleteData(
+        `${process.env.REACT_APP_API_URL}/api/student/${deleteID}`
+      );
+      setRefresh(true);
+      handleClose();
+    }
+    removeData();
+  };
+
   return (
     <div className="studentPageContainer">
       <h2 className="title">Students</h2>
@@ -38,7 +86,39 @@ const StudentPage = () => {
             Create New Student
           </Button>
         </div>
-        <Table data={students} type="student" />
+        <Table
+          data={students}
+          type="student"
+          setDeleteID={setDeleteID}
+          openModal={handleOpen}
+        />
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className="deleteModal">
+              <h3 className="title">To confirm delete, type 'delete' below:</h3>
+              <TextField
+                className="text"
+                id="standard-basic"
+                variant="standard"
+                sx={{ mb: 2, mt: 2 }}
+                value={deleteText}
+                onChange={(e) => setDeleteText(e.target.value)}
+              />
+              <Button
+                variant="outlined"
+                color="error"
+                onClick={handleConfirmDelete}
+              >
+                CONFIRM
+              </Button>
+            </div>
+          </Box>
+        </Modal>
       </div>
     </div>
   );
