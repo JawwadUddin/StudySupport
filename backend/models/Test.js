a92d14e59372e3636ddcbd7b805ee8c4519bb60c
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-11-03 16:59:02 +0000

    create test model

---
+++ backend/models/Test.js
@@ -0,0 +1,98 @@
+const sql = require("mssql");
+const { dbConnect } = require("../db");
+
+class Test {
+  constructor(data) {
+    this.id = data.test_id;
+    this.testName = data.test_name;
+    this.syllabusID = data.syllabus_id;
+    this.syllabusName = data.syllabus_name;
+    this.questionID = data.question_id;
+    this.topicID = data.topic_id;
+    this.topicName = data.topic_name;
+    this.difficulty = data.difficulty;
+    this.marks = data.marks;
+  }
+
+  static get all() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await dbConnect();
+        const testData = await pool.request().execute("SelectAllTests");
+        const tests = testData.recordset.map((d) => new Test(d));
+        resolve(tests);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  static findByID(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await dbConnect();
+        const testData = await pool
+          .request()
+          .input("TestID", sql.Int, id)
+          .execute("SelectQuestionsForTest");
+        const test = testData.recordset.map((d) => new Test(d));
+        resolve(test);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  static studentTestsCompleted(studentID) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await dbConnect();
+        const testData = await pool
+          .request()
+          .input("StudentID", sql.Int, studentID)
+          .execute("SelectStudentTestsCompleted");
+        const test = testData.recordset.map((d) => new Test(d));
+        resolve(test);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  static create(test) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await dbConnect();
+        const testData = await pool
+          .request()
+          .input("TestName", sql.VarChar, test.testName)
+          .input("SyllabusID", sql.Int, test.syllabusID)
+          .input("JsonQuestions", sql.VarChar, JSON.stringify(test.questions))
+          .output("TestID", sql.Int)
+          .execute("InsertTest");
+        const newTest = testData.output.TestID;
+        resolve({ newTestID: newTest });
+      } catch (err) {
+        reject("Error creating Test: " + err.message);
+      }
+    });
+  }
+
+  static edit(id, test) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await dbConnect();
+        const testData = await pool
+          .request()
+          .input("TestID", sql.Int, id)
+          .input("TestName", sql.VarChar, test.testName)
+          .execute("UpdateTest");
+        resolve("Test successfully updated");
+      } catch (err) {
+        reject("Error updating student: " + err.message);
+      }
+    });
+  }
+}
+
+module.exports = Test;
