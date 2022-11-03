8539f7b7ffa21a177067cb87c28aa5b47821c9a7
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-11-03 16:59:25 +0000

    test routing

---
+++ backend/routes/test.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const testController = require("../controllers/test");
+
+router.get("/", testController.index);
+router.get("/:id", testController.show);
+router.get("/completed/:id", testController.studentCompleted);
+router.post("/", testController.create);
+router.patch("/:id", testController.update);
+
+module.exports = router;
