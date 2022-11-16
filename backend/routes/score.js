1f08c4ce32d8f2aba5a6e84a0b01068e56b1c15a
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-11-16 17:59:28 +0000

    add testComment to score controller

--- backend/routes/score.js
+++ backend/routes/score.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const scoreController = require("../controllers/score");
 
 router.get("/:studentID/:testID", scoreController.show);
-router.get("/", scoreController.show);
 router.post("/", scoreController.create);
 router.patch("/", scoreController.update);
 
