eed2d91b5908a2f7c40c054aae3be03938836611
JawwadUddin <m.m.j.uddin12@gmail.com>
2022-11-27 00:22:57 +0000

    edit score functionality frontend

--- frontend/src/pages/scorePage/ScorePage.jsx
+++ frontend/src/pages/scorePage/ScorePage.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import "./scorePage.css";
 import { useParams, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { getData } from "../../helpers/apiFunctions";
+import { getData, updateData } from "../../helpers/apiFunctions";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
 import { status } from "../../helpers/status";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 
 const ScorePage = () => {
   let { studentID, testName } = useParams();
   const location = useLocation();
+  const [currentlyEditing, setCurrentlyEditing] = useState(false);
+  const [marksReceived, setMarksReceived] = useState("");
   const testID = location.state ? location.state.testID : null;
   const [scores, setScores] = useState([]);
   const [testComment, setTestComment] = useState({});
+  const [refresh, setRefresh] = useState(true);
 
   useEffect(() => {
     try {
@@ -24,17 +31,69 @@ const ScorePage = () => {
           `${process.env.REACT_APP_API_URL}/api/score/${studentID}/${testID}`
         );
         if (serverResponse.message === "OK") {
+          setRefresh(false);
           setScores(serverResponse.results.data.scores);
           setTestComment(serverResponse.results.data.testComment);
         } else {
           throw Error(serverResponse.message);
         }
       }
-      fetchData();
+      if (refresh) {
+        fetchData();
+      }
     } catch (error) {
       console.log(error);
     }
-  }, [studentID, testID]);
+  }, [studentID, testID, refresh]);
+
+  const editScoreMode = (score) => {
+    setCurrentlyEditing(true);
+    setMarksReceived(score.marksReceived);
+    const updatedScoreArray = scores.map((item) => {
+      if (item.id !== score.id) {
+        return item;
+      } else {
+        return { ...score, edit: true };
+      }
+    });
+    setScores((prev) => updatedScoreArray);
+  };
+
+  const cancelEdit = (score) => {
+    const updatedScoreArray = scores.map((item) => {
+      if (item.id !== score.id) {
+        return item;
+      } else {
+        return { ...score, edit: false };
+      }
+    });
+    setScores((prev) => updatedScoreArray);
+    setCurrentlyEditing(false);
+  };
+
+  const editScore = (score) => {
+    try {
+      async function editData() {
+        if (marksReceived === "") return;
+        console.log(score.id, marksReceived);
+        const serverResponse = await updateData(
+          `${process.env.REACT_APP_API_URL}/api/score/${score.id}`,
+          {
+            marksReceived: marksReceived,
+          }
+        );
+        if (serverResponse.message === "OK") {
+          setCurrentlyEditing(false);
+          setRefresh(true);
+        } else {
+          throw Error(serverResponse.message);
+        }
+      }
+      editData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="scorePageContainer">
@@ -43,18 +102,6 @@ const ScorePage = () => {
         <div className="listContainer">
           <div className="listHeader">
             <div className="listTitle">Score Information</div>
-            {/* <Button
-              onClick={() =>
-                navigate(`edit`, {
-                  state: { scoreInfo: scores },
-                })
-              }
-              variant="outlined"
-              color="secondary"
-              className="editBtn"
-            >
-              Edit Scores
-            </Button> */}
           </div>
           <Table>
             <TableHead>
@@ -73,7 +120,49 @@ const ScorePage = () => {
                     <TableCell>{index + 1}</TableCell>
                     <TableCell>{score.topicName}</TableCell>
                     <TableCell>{score.difficulty}</TableCell>
-                    <TableCell>{`${score.marksReceived} / ${score.marks}`}</TableCell>
+                    {score.edit ? (
+                      <TableCell>
+                        <input
+                          className="scoreInput"
+                          type="number"
+                          value={marksReceived}
+                          onChange={(e) => setMarksReceived(e.target.value)}
+                        ></input>{" "}
+                        {" / " + score.marks}
+                      </TableCell>
+                    ) : (
+                      <TableCell>{`${score.marksReceived} / ${score.marks}`}</TableCell>
+                    )}
+                    {score.edit ? (
+                      <TableCell align="right">
+                        <IconButton
+                          onClick={() => cancelEdit(score)}
+                          aria-label="cancel-edit"
+                          color="error"
+                        >
+                          <CloseIcon />
+                        </IconButton>
+                        <IconButton
+                          onClick={() => editScore(score)}
+                          aria-label="confirm-edit"
+                          color="success"
+                        >
+                          <CheckIcon />
+                        </IconButton>
+                      </TableCell>
+                    ) : (
+                      <TableCell align="right">
+                        <IconButton
+                          onClick={() =>
+                            !currentlyEditing && editScoreMode(score)
+                          }
+                          aria-label="edit"
+                          color="secondary"
+                        >
+                          <EditIcon />
+                        </IconButton>
+                      </TableCell>
+                    )}
                   </TableRow>
                 );
               })}
