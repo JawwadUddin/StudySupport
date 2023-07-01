19d66c03be82056162468fe7c9ac60e78fabdafb
JawwadUddin <m.m.j.uddin12@gmail.com>
2023-07-01 02:15:38 +0100

    backend login and jwt

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const sql = require("mssql");
+const { dbConnect } = require("./db");
 const server = express();
+const bcrypt = require("bcrypt");
+const { success, error } = require("./helper/responseApi");
+const jwt = require("jsonwebtoken");
+const secretKey = "your_secret_key";
 
 server.use(cors());
 server.use(express.json());
@@ -42,6 +47,58 @@ server.get("/", (req, res) => {
   res
     .status(200)
     .json({ message: "Welcome to the backend server for Study Support" });
+});
+
+// server.post("/register", async (req, res) => {
+//   const { username, password } = req.body;
+
+//   try {
+//     // Generate a salt and hash the password
+//     const saltRounds = 10;
+//     const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+//     console.log(hashedPassword);
+
+//     res.status(200).json({ message: "Registration successful" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "An error occurred" });
+//   }
+// });
+
+server.post("/login", async (req, res) => {
+  const { username, password } = req.body.data;
+
+  try {
+    const pool = await dbConnect();
+    const userData = await pool
+      .request()
+      .input("Username", sql.VarChar, username)
+      .output("HashedPassword", sql.VarChar)
+      .execute("Login");
+    const hashedPassword = userData.output.HashedPassword;
+    if (!hashedPassword) {
+      // If the user is not found, return an error response
+      res.status(401).json({ message: "Invalid username or password" });
+      return;
+    }
+
+    // Compare the provided password with the stored hashed password
+    const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+
+    if (isPasswordValid) {
+      const token = jwt.sign({ username }, secretKey);
+
+      // If the password matches, authentication is successful
+      res.status(200).json(success("OK", { data: token }, res.statusCode));
+    } else {
+      // If the password does not match, return an error response
+      res.status(401).json({ message: "Invalid username or password" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
 });
 
 module.exports = server;
