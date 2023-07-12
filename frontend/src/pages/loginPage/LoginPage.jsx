721f8a5fc286af705540b713d19e666a628cf6c1
JawwadUddin <m.m.j.uddin12@gmail.com>
2023-07-13 00:48:12 +0100

    login page error handling

--- frontend/src/pages/loginPage/LoginPage.jsx
+++ frontend/src/pages/loginPage/LoginPage.jsx
@@ -3,26 +3,31 @@ import "./loginPage.css";
 import { saveData } from "../../helpers/apiFunctions";
 import { useSignIn } from "react-auth-kit";
 import { useNavigate } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const LoginPage = () => {
   const signIn = useSignIn();
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault();
     try {
       if (username === "" || password === "") {
         console.log("Form not complete");
         return;
       }
       async function submitData() {
+        setLoading(true);
         const serverResponse = await saveData(
           `${process.env.REACT_APP_API_URL}/login`,
           { username, password }
         );
+        setLoading(false);
         if (serverResponse.message === "OK") {
-          console.log("yes");
           if (
             signIn({
               token: serverResponse.results.data,
@@ -32,41 +37,52 @@ const LoginPage = () => {
             })
           ) {
             navigate("/");
-          } else console.log("cannot sign you in");
+          } else {
+            throw Error({ message: "Error with Sign In Package" });
+          }
         } else {
-          console.log(serverResponse);
           throw Error(serverResponse.message);
         }
       }
-      submitData();
-    } catch (error) {
-      console.log(error);
+      submitData().catch((error) => {
+        setError(error.message);
+      });
+    } catch (err) {
+      console.log("here");
+      setError(err.message);
     }
   }
 
   return (
     <div className="loginPageContainer">
       <div class="login-box">
-        <h2>Login</h2>
-        <div class="user-box">
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label>Username</label>
-        </div>
-        <div class="user-box">
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label>Password</label>
-        </div>
-        <div className="login-submit">
-          <button onClick={() => handleSubmit()}>Submit</button>
-        </div>
+        <form>
+          <h2>Login</h2>
+          {error ? <div className="error-message">{error}</div> : null}
+          <div class="user-box">
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label>Username</label>
+          </div>
+          <div class="user-box">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label>Password</label>
+          </div>
+          <div className="login-submit">
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              <button onClick={(e) => handleSubmit(e)}>Submit</button>
+            )}
+          </div>
+        </form>
       </div>
     </div>
   );
