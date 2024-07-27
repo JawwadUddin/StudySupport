535381dd9e6a5df179df1446686486aa47c47b81
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-07-27 01:59:47 +0100

    Add report section to dashboard

--- frontend/src/pages/dashboardPage/DashboardPage.jsx
+++ frontend/src/pages/dashboardPage/DashboardPage.jsx
@@ -1,5 +1,36 @@
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import ExcelReport from "../../components/excelReport/ExcelReport";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12} md={8} lg={6}>
+          <div className="listContainer">
+            <div className="listHeader">
+              <div className="listTitle">Reports</div>
+            </div>
+            <ExcelReport />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={8} lg={6}>
+          <div className="listContainer">
+            <div className="listHeader">
+              <div className="listTitle">Sales</div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="listContainer">
+            <div className="listHeader">
+              <div className="listTitle">Score History</div>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default DashboardPage;
