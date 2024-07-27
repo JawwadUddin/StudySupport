99689b024e38be5a68fc9fbb23da93d72bbf541b
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-07-27 22:44:07 +0100

    Add @mui/x-chart

--- frontend/src/pages/dashboardPage/DashboardPage.jsx
+++ frontend/src/pages/dashboardPage/DashboardPage.jsx
@@ -1,6 +1,6 @@
-import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import ExcelReport from "../../components/excelReport/ExcelReport";
+import { LineChart } from "@mui/x-charts";
 
 const DashboardPage = () => {
   return (
@@ -14,20 +14,30 @@ const DashboardPage = () => {
             <ExcelReport />
           </div>
         </Grid>
-        <Grid item xs={12} md={8} lg={6}>
+        {/* <Grid item xs={12} md={8} lg={6}>
           <div className="listContainer">
             <div className="listHeader">
               <div className="listTitle">Sales</div>
             </div>
+            <LineChart
+              xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+              series={[
+                {
+                  data: [2, 5.5, 2, 8.5, 1.5, 5],
+                },
+              ]}
+              width={500}
+              height={300}
+            />
           </div>
-        </Grid>
-        <Grid item xs={12}>
+        </Grid> */}
+        {/* <Grid item xs={12}>
           <div className="listContainer">
             <div className="listHeader">
-              <div className="listTitle">Score History</div>
+              <div className="listTitle"></div>
             </div>
           </div>
-        </Grid>
+        </Grid> */}
       </Grid>
     </>
   );
